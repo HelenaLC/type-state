52078f28f4eb420b97779329c160fe23d364522a
HelenaLC <helena@crowells.eu>
2023-05-24 15:56:59 +0200

    init Snakemake setup

--- code/scripts/00-get_data.R
+++ code/scripts/00-get_data.R
@@ -5,17 +5,17 @@ x <- fun()
 # number of features & observations
 dim(x)
 
-s <- !is.null(x$sample_id)
-k <- !is.null(x$cluster_id)
-if (b) {
-  table(x$sample_id)
-  if (k) {
-    table(x$cluster_id)
-    table(x$sample_id, x$cluster_id)
-  }
-} else if (k) {
-  table(x$cluster)
-}
+# s <- !is.null(x$sample_id)
+# k <- !is.null(x$cluster_id)
+# if (b) {
+#   table(x$sample_id)
+#   if (k) {
+#     table(x$cluster_id)
+#     table(x$sample_id, x$cluster_id)
+#   }
+# } else if (k) {
+#   table(x$cluster)
+# }
 
 dimnames(x) <- list(
   paste0("gene", seq_len(nrow(x))),
