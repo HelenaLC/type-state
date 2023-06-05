09107fa42c281224ab01ee4fc411d3e55252d998
HelenaLC <helena@crowells.eu>
2023-06-05 12:22:57 +0200

    add feature selection

--- code/scripts/01-fil_data_splatter.R
+++ code/scripts/01-fil_data_splatter.R
@@ -7,10 +7,11 @@ suppressPackageStartupMessages({
 # read in raw data
 x <- readRDS(args[[1]])
 
-colData(x) <- DataFrame(sample_id = x$Sample,
-                                cluster_id = x$Group,
-                                condition = x$Condition,
-                                row.names = NULL)
+colData(x) <- DataFrame(
+    sample_id = x$Sample,
+    cluster_id = x$Group,
+    condition = x$Condition,
+    row.names = NULL)
 
 by <- c("cluster_id", "sample_id", "condition")
 by <- intersect(by, names(colData(x)))
@@ -20,8 +21,15 @@ x <- x[
   rowSums(counts(x) > 1) >= 10,
   colSums(counts(x) > 0) >= 10]
 
+# normalization
 x <- logNormCounts(x)
-x <- runPCA(x, ncomponents = 10)
+
+# feature selection
+tbl <- modelGeneVar(x, block = x$sample_id)
+hvg <- tbl$bio > 0
+
+# dimension reduction
+x <- runPCA(x, ncomponents = 10, subset_row = hvg)
 #x$cluster_id <- quickCluster(x, method = "igraph", graph.fun = "louvain", k = 5)
 
 # drop missing factor levels
