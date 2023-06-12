dc6d366057fc46507e0c5b337b426ef519ae0de2
Jiayi-Wang-Joey <591813079@qq.com>
2023-06-12 20:22:33 +0200

    roc

--- code/03-sel.R
+++ code/03-sel.R
@@ -6,10 +6,11 @@ source(args[[1]])
 res <- lapply(args[[2]], readRDS)
 res <- do.call("rbind", res)
 
+
 res <- data.frame(res, wcs)
 #res$sel_val <- fun(res$sco_val)
-idx <- fun(res$sco_val, method = res$sco[1])
-res$sel_val <- FALSE
-res$sel_val[idx] <- TRUE
+#idx <- fun(res$sco_val, method = res$sco[1])
+#res$sel_val <- FALSE
+#res$sel_val[idx] <- TRUE
 
 saveRDS(res, args[[3]])
