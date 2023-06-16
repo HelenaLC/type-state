4d1116ac21ad761253c233510eced40e0519b70f
Jiayi-Wang-Joey <591813079@qq.com>
2023-06-16 12:53:12 +0200

    sel

--- code/03-sel-topFstat.R
+++ code/03-sel-topFstat.R
@@ -1,6 +1,6 @@
 fun <- \(x) {
     #y <- x$sco_val[x$sco == "entropy"]
-    y <- x[["type_entropy"]]
+    y <- x[["type_Fstat"]]
     o <- order(y$sco_val, decreasing = FALSE)
-    o <= round(length(y)*0.3)
+    o <= round(length(y)*0.25)
 }
