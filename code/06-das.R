26e30bd27ce2bac446e9c0f3e13de292c2edeacf
HelenaLC <helena@crowells.eu>
2023-06-19 15:00:57 +0200

    add das

--- code/06-das.R
+++ code/06-das.R
@@ -7,9 +7,16 @@ source(args[[1]])
 sce <- readRDS(args[[2]])
 
 res <- fun(sce)
-if (!is.null(res))
+if (!is.null(res)) {
     res <- data.frame(
         row.names = NULL, 
         metadata(sce), wcs, res)
+    nan <- setdiff(res$gene, rownames(sce))
+    if (length(nan)) {
+        kid <- levels(sce$cluster_id)
+        add <- expand.grid(gene = nan, cluster_id = kid)
+        res <- dplyr::bind_rows(res, add)
+    }
+}
 
 saveRDS(res, args[[3]])
