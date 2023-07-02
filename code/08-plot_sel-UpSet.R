bbe134aef8db529d45be965de6f07eca80285784
Jiayi-Wang-Joey <591813079@qq.com>
2023-07-02 17:02:21 +0200

    update upset

--- code/08-plot_sel-UpSet.R
+++ code/08-plot_sel-UpSet.R
@@ -10,23 +10,21 @@ suppressPackageStartupMessages({
 sel <- lapply(args[[1]], readRDS)
 names(sel) <- sapply(sel, \(.) .$sel[1])
 df <- do.call(rbind, sel) 
-
+df$info <- paste0("t",df$t,",s",df$s)
 
 plt <- list()
-for (t in unique(df$t)) {
-    for(s in unique(df$s)){
-        tmp <- df[df$t == t & df$s == s,]
-        lst <- split(tmp, tmp$sel)
-        sel_idx <- lapply(lst, \(x) x$gene_id[x$sel_val == TRUE])
-        plt[[paste0("t",t,",s",s)]] <-
-            as.ggplot(upset(fromList(sel_idx),
-                order.by = "freq",
-                nsets = length(sel_idx),
-                set_size.show = TRUE,
-                scale.sets = "identity"))
-        
-    }
-}
+plt <- lapply(unique(df$info), \(ts){
+    tmp <- df[df$info == ts,]
+    lst <- split(tmp, tmp$sel)
+    sel_idx <- lapply(lst, \(x) x$gene_id[x$sel_val == TRUE])
+    p <- as.ggplot(upset(fromList(sel_idx),
+        order.by = "freq",
+        nsets = length(sel_idx),
+        set_size.show = TRUE,
+        scale.sets = "identity"))
+})
+names(plt) <- unique(df$info)
+
 
 
 res <- plot_grid(plotlist = plt, labels = names(plt), ncol = 6, nrow = 6)
