d442e333f5f77da3e08a891377b95e01bb84e96e
Jiayi-Wang-Joey <591813079@qq.com>
2023-12-10 12:33:29 +0100

    add qlt

--- code/06-das-DS_lemur.R
+++ code/06-das-DS_lemur.R
@@ -5,7 +5,7 @@ suppressPackageStartupMessages({
 fun <- \(x) {
     ids <- levels(x$group_id)
     fit <- lemur(x, design=~group_id, verbose=FALSE)
-    #fit <- align_harmony(fit, verbose=FALSE)
+    fit <- align_harmony(fit, verbose=FALSE)
     fit <- test_de(fit, cond(group_id=ids[1])-cond(group_id=ids[2]))
     res <- find_de_neighborhoods(fit, vars(sample_id, group_id), verbose=FALSE)
     old <- c("name", "neighborhood", "pval", "adj_pval")
